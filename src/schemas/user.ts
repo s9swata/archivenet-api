c5570f2d9af28cc30e6e6e297c4b9ed42beb6b13
Saswata Biswas <saswata.biswas20@gmail.com>
2025-06-05 13:48:19 +0530

    fix file structure

---
+++ src/schemas/user.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+import { apiKeySchema } from './apiKey.js';
+import { subscriptionSchema } from './subscriptions.js';
+
+
+export const userSchema = z.object({
+    id: z.string().uuid(),
+    username: z.string().min(3).max(30),
+    email: z.string().email(),
+    password: z.string().min(8).max(100),
+    createdAt: z.coerce.date(),
+    updatedAt: z.coerce.date().optional(),
+    apiKeys: z.array(apiKeySchema).optional(), // Optional array of API keys associated with the user
+    subscriptionPlans: z.array(subscriptionSchema).optional(), // Optional array of subscriptions associated with the user
+})
+export type User = z.infer<typeof userSchema>;
